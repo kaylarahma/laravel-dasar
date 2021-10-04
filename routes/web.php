926764d4c2109df909b278f691bdd12c76834bc6
kaylarahma <kaylarahmanisa83@gmail.com>
2021-10-04 14:30:11 +0700

    nambah parameter wajib dan optional

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,51 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('nama', function () {
+    return 'Nama Saya Kayla Rahmanisa';
+});
+
+Route::get('umur', function () {
+    $umur = 17;
+    return 'Saya berumur ' . $umur;
+});
+
+Route::get('alamat', function () {
+    return 'Rumah Saya di Jl.Sukamenak';
+});
+
+Route::get('skul', function () {
+    return 'Saya Bersekolah di SMK ASSALAAM BANDUNG';
+});
+
+Route::get('sekian', function () {
+    return 'Sekian Wassalaam';
+});
+
+//parameter wajib
+Route::get('/aku/riwayat/{riwayat}', function ($riwayat) {
+    return $riwayat . ' anak ke 3 dari 4 bersaudara';
+});
+
+//parameter wajib
+Route::get('/aku/riwayat/{nama}/{sekola}', function ($nama, $sekola) {
+    return 'Nama saya : ' . $nama . '<br>Saya bersekolah di ' . $sekola;
+});
+
+//parameter optional kalo tidak diisi
+Route::get('/nama-saya-adalah-{nama?}', function ($nama = null) {
+    return 'Nama Saya Adalah : ' . $nama;
+});
+
+//parameter optional kalo di isi
+Route::get('/nama-saya-adalah/{nama?}', function ($nama = 'NAMA WAJIB DI ISI!!') {
+    return 'Nama Saya Adalah : ' . $nama;
+});
+
+//LATIHAN
+Route::get('/hitung-luas-segitiga/{alas}/{tinggi}', function ($alas, $tinggi) {
+    $hasil = $alas * $tinggi * 0.5;
+    return 'Hasil Luas Segitiga : ' . $hasil;
 });
